6ab41ed3c299e3b09278dee63643ad8e21ee02da
Patrick Crawford <patrick.crawford@senecacollege.ca>
2023-11-13 23:00:01 -0500

    updated content matching pattern

--- TailwindCSS/tailwind.config.js
+++ TailwindCSS/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [`./views/*.html`], // all .html files
+  content: [`./views/**/*.html`], // all .html files
   theme: {
     extend: {},
   },
